72ad68672d06c7c26bb3338b1a0276816d72c9ba
Mateus Koehler <mateus@mateuskoehler.com>
2019-06-03 14:26:29 -0400

    up to section 23

---
+++ Section-23/translate/src/components/Button.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class Button extends React.Component {
+  render() {
+    //Helper method way
+    // renderSubmit(value) {
+    //   return value === 'english' ? 'Submit' : 'Voorleggen'
+    // }
+
+    return (
+      <ColorContext.Consumer>
+        {color => (
+          <button className={`ui button ${color}`}>
+            <LanguageContext.Consumer>
+              {value => (value === 'english' ? 'Submit' : 'Voorleggen')}
+            </LanguageContext.Consumer>
+          </button>
+        )}
+      </ColorContext.Consumer>
+    );
+  }
+}
+
+export default Button;
---
+++ Section-23/translate/src/contexts/LanguageContext.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const context = React.createContext('english');
+
+console.log(context);
+
+export default context;
